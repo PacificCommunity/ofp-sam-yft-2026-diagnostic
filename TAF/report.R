c8142b4077298d3843b4b4f42177f54302e26f85
Arni Magnusson <thisisarni@gmail.com>
2026-09-09 17:13:07 +1100

    Remove extra space

--- TAF/report.R
+++ TAF/report.R
@@ -1,7 +1,7 @@
 # Prepare plots and tables for report
 
 # Before: cpue.csv (data), biology.csv, biomass.csv, catch.csv, cpue.csv,
-#          f_annual.csv, f_stage.csv, summary.csv (output)
+#         f_annual.csv, f_stage.csv, summary.csv (output)
 # After:  biology.csv, f_adult_juvenile_same_free.png,
 #         f_adult_juvenile_same_axes.png, f_last_10_free_axes.png,
 #         f_last_10_same_axes, summary.csv (report)
